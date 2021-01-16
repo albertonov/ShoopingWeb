a2c5d62faf2eb5496a5ec2189b930e8603d4a569
Alberto Novillo <albertonov98@gmail.com>
2021-01-16 21:32:04 +0100

    All files revised. Db complete!

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ var uri = 'mongodb://localhost/gameshop';
 mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
-  db.on('connecting', function () { console.log('Connecting to ', uri); });
-  db.on('connected', function () { console.log('Connected to ', uri); });
-  db.on('disconnecting', function () { console.log('Disconnecting from ', uri); });
-  db.on('disconnected', function () { console.log('Disconnected from ', uri); });
-  db.on('error', function (err) { console.error('Error ', err.message); });
+db.on('connecting', function () { console.log('Connecting to ', uri); });
+db.on('connected', function () { console.log('Connected to ', uri); });
+db.on('disconnecting', function () { console.log('Disconnecting from ', uri); });
+db.on('disconnected', function () { console.log('Disconnected from ', uri); });
+db.on('error', function (err) { console.error('Error ', err.message); });
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
@@ -51,12 +51,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/products', function (req, res, next) {
   return model.getProducts()
-  .then(function (products) {
-    if (products) {
-      return res.json(products);
-    }
-    else return res.status(401).send({ message: 'Cannot get products' });
-  })
+    .then(function (products) {
+      if (products) {
+        return res.json(products);
+      }
+      else return res.status(401).send({ message: 'Cannot get products' });
+    })
 });
 
 // Adds the / path to the application
@@ -74,7 +74,7 @@ app.listen(3000, function () {
 });
 
 app.post('/api/users/signin', function (req, res, next) {
-  
+
   return model.signin(req.body.email, req.body.password)
     .then(function (user) {
       if (user) {
@@ -89,31 +89,31 @@ app.post('/api/users/signin', function (req, res, next) {
 app.post('/api/users/signup', function (req, res, next) {
   console.log(req.body.birth)
   return model.signup(req.body.name, req.body.surname, req.body.address, req.body.birth, req.body.email, req.body.password)
-  .then(function (user) {
-  if (user) return res.json(user);
-  else return res.status(401).send({ message: 'Invalid email' });
-  })
-  });
+    .then(function (user) {
+      if (user) return res.json(user);
+      else return res.status(401).send({ message: 'Invalid email' });
+    })
+});
 
 
-  app.get('/api/cart', function (req, res, next) {
+app.get('/api/cart', function (req, res, next) {
 
-    var uid = req.cookies.userid;
-    return model.getCartByUserId(uid)
-      .then(function(cart){
-        if (cart) { return res.json(cart); }
-        else return res.status(401).send({ message: 'User shopping cart not found' });
-      })
+  var uid = req.cookies.userid;
+  return model.getCartByUserId(uid)
+    .then(function (cart) {
+      if (cart) { return res.json(cart); }
+      else return res.status(401).send({ message: 'User shopping cart not found' });
+    })
 
-  });
+});
 
-  app.delete('/api/cart/items/product/:id/one',
+app.delete('/api/cart/items/product/:id/one',
   function (req, res, next) {
 
     var pid = req.params.id;
     var uid = req.cookies.userid;
     return model.removeOne(uid, pid)
-      .then(function(result){
+      .then(function (result) {
         if (result) { return res.json(result); }
         else return res.status(401).send(
           { message: 'User or Product not found' });
@@ -121,14 +121,14 @@ app.post('/api/users/signup', function (req, res, next) {
 
   });
 
-  app.delete('/api/cart/items/product/:id/all',
+app.delete('/api/cart/items/product/:id/all',
   function (req, res, next) {
 
     var pid = req.params.id;
     var uid = req.cookies.userid;
 
     return model.removeAll(uid, pid)
-      .then(function(result){
+      .then(function (result) {
         if (result) { return res.json(result); }
         else return res.status(401).send(
           { message: 'User or Product not found' });
@@ -137,25 +137,25 @@ app.post('/api/users/signup', function (req, res, next) {
 
 
 
-  app.get('/api/cart/qty', function (req, res, next) {
-    var userid = req.cookies.userid;
-    if (!userid) return res.status(401).send({ message: 'User has not signed in' });
-    return model.getUserCartQty(userid)
-      .then(function (userCartQty) {
-        if (userCartQty) return res.json(userCartQty);
-        else return res.status(500).send({ message: 'Cannot retrieve user cart quantity' });
-      })
-  });
+app.get('/api/cart/qty', function (req, res, next) {
+  var userid = req.cookies.userid;
+  if (!userid) return res.status(401).send({ message: 'User has not signed in' });
+  return model.getUserCartQty(userid)
+    .then(function (userCartQty) {
+      if (userCartQty) return res.json(userCartQty);
+      else return res.status(500).send({ message: 'Cannot retrieve user cart quantity' });
+    })
+});
 
-  app.post('/api/cart/items/product/:id', function (req, res, next) {
-    var pid = req.params.id;
-    var uid = req.cookies.userid;
-    return model.buy(uid, pid)
-      .then(function (cart) {
-        if (cart) return res.json(cart);
-        else return res.status(401).send({ message: 'User or Product not found' });
-      })
-  });
+app.post('/api/cart/items/product/:id', function (req, res, next) {
+  var pid = req.params.id;
+  var uid = req.cookies.userid;
+  return model.buy(uid, pid)
+    .then(function (cart) {
+      if (cart) return res.json(cart);
+      else return res.status(401).send({ message: 'User or Product not found' });
+    })
+});
 
 app.post('/api/orders',
   function (req, res, next) {
@@ -165,25 +165,25 @@ app.post('/api/orders',
     var cardNumber = req.body.cardNumber;
     var cardOwner = req.body.cardOwner;
     var uid = req.cookies.userid;
-    
+
     return model.purchase(date, address, cardNumber, cardOwner, uid)
-    .then(function (order){
-      
-      return model.resetCart(uid)
-      .then( function(user){
-        
-        return model.getUserByIdWithCart(uid)
-        .then( function(user){
-          user.orders.push(order)
-          console.log("limpio")
-          return user.save()
-          .then(function (result) {
-            if (result) return res.json(order);
-            else return res.status(401).send({ message: 'User or Product not found' });
+      .then(function (order) {
+
+        return model.resetCart(uid)
+          .then(function (user) {
+
+            return model.getUserByIdWithCart(uid)
+              .then(function (user) {
+                user.orders.push(order)
+                console.log("limpio")
+                return user.save()
+                  .then(function (result) {
+                    if (result) return res.json(order);
+                    else return res.status(401).send({ message: 'User or Product not found' });
+                  })
+              })
           })
-        })
-      })
-    }).catch(function (errors) { console.error(errors); return null; })
+      }).catch(function (errors) { console.error(errors); return null; })
 
 
   });
@@ -200,7 +200,7 @@ app.post('/api/orders',
 
 
 app.get('/api/orders', function (req, res, next) {
-  
+
   var uid = req.cookies.userid;
   var orders = model.getOrdersByUserId(uid);
   if (orders) { return res.json(orders); }
@@ -210,25 +210,24 @@ app.get('/api/orders', function (req, res, next) {
 
 app.get('/api/profile', function (req, res, next) {
   return model.getUserByIdWithOrders(req.cookies.userid)
-  .then(function (profile) {
-    if (profile) {console.log(profile); return res.json(profile); }
-  else return res.status(401).send({ message: 'User  not found' });
-  })
-  
+    .then(function (profile) {
+      if (profile) { console.log(profile); return res.json(profile); }
+      else return res.status(401).send({ message: 'User  not found' });
+    })
+
 });
 
 app.get('/api/orders/id/:oid', function (req, res, next) {
-  //obtener las orders del usuario; obtener la orden que coincida el number con el ID; delvolver esa order
   var oid = req.params.oid;
   console.log(oid)
   return model.getOrder(oid, req.cookies.userid)
-  .then(function (order){
-    if (order) {
-      console.log(order)
-      return res.json(order);
-    }
-    else return res.status(401).send({ message: 'Order  not found' });
-  })
+    .then(function (order) {
+      if (order) {
+        console.log(order)
+        return res.json(order);
+      }
+      else return res.status(401).send({ message: 'Order  not found' });
+    })
 
 });
 
--- model/model.js
+++ model/model.js
@@ -3,134 +3,18 @@ var Cart = require('./shopping-cart')
 var Product = require('./product');
 var Order = require('./order');
 var Model = {}
-
-/*
-Model.products = [
-  {
-    _id: 1,
-    title: 'Microsoft Surface Pro 7',
-    url: 'images/sfp.png',
-    description: 'Ultra-light and versatile. At your desk, on the couch, or in the yard, get more done your way with Surface Pro 7, featuring a laptop-class Intel® Core™ processor, all-day battery,¹ and HD cameras.',
-    price: 999.99,
-    tam: '12.3" Full HD 1920 x 1080',
-    procesador: 'Intel Core i51035G4 1.1GHz' ,
-    ram: '8Gb RAM LPDDR4X' ,
-    disco: '128 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 2,
-    title: 'Apple MacBook Air',
-    url: 'images/mas.png',
-    description: 'The Most Popular Mac Now Has Up to 2x Faster Performance. The incredibly thin and light MacBook Air is now more powerful than ever. It features a brilliant Retina display, new Magic Keyboard and Touch ID.',
-    price: 1099.99,
-    tam: '12.1" 4k 1920 x 1080',
-    procesador: 'Intel Core i51335G4 3.4GHz' ,
-    ram: '32Gb RAM LPDDR4X' ,
-    disco: '512 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 3,
-    title: 'Lenovo IdeaPad 5',
-    url: 'images/lip.png',
-    description: 'Designed for working in bright-light conditions, the large 14” narrow 7.3mm display of the Acer Chromebook 314 is ideal for greater productivity. Due his long battery life you can do everything.',
-    price: 899.99,
-    tam: '16.8" 1920 x 1080',
-    procesador: 'Intel Core i51035G4 3GHz' ,
-    ram: '16Gb RAM LPDD' ,
-    disco: '256 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 4,
-    title: 'Acer Chromebook 314',
-    url: 'images/acb.png',
-    description: 'Designed for working in bright-light conditions, the large 14” narrow 7.3mm display of the Acer Chromebook 314 is ideal for greater productivity. Due his long battery life you can do everything.',
-    price: 999.99,
-    tam: '21.3" Full HD 1920 x 1080',
-    procesador: 'Intel Core i51035G4 1.1GHz' ,
-    ram: '32Gb RAM ' ,
-    disco: '128 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 5,
-    title: 'Asus XV550',
-    url: 'images/sfp.png',
-    description: 'Ultra-light and versatile. At your desk, on the couch, or in the yard, get more done your way with Surface Pro 7, featuring a laptop-class Intel® Core™ processor, all-day battery,¹ and HD cameras.',
-    price: 999.99,
-    tam: '12.3" Full HD 1920 x 1080',
-    procesador: 'Intel Core i51035G4 1.1GHz' ,
-    ram: '8Gb RAM LPDDR4X' ,
-    disco: '128 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 6,
-    title: 'HP pavilion Laptop',
-    url: 'images/mas.png',
-    description: 'The Most Popular Mac Now Has Up to 2x Faster Performance. The incredibly thin and light MacBook Air is now more powerful than ever. It features a brilliant Retina display, new Magic Keyboard and Touch ID.',
-    price: 1099.99,
-    tam: '12.1" 4k 1920 x 1080',
-    procesador: 'Intel Core i51335G4 3.4GHz' ,
-    ram: '32Gb RAM LPDDR4X' ,
-    disco: '512 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 7,
-    title: 'Lenovo SmartPad',
-    url: 'images/lip.png',
-    description: 'Designed for working in bright-light conditions, the large 14” narrow 7.3mm display of the Acer Chromebook 314 is ideal for greater productivity. Due his long battery life you can do everything.',
-    price: 899.99,
-    tam: '16.8" 1920 x 1080',
-    procesador: 'Intel Core i51035G4 3GHz' ,
-    ram: '16Gb RAM LPDD' ,
-    disco: '256 GB SSD',
-    qtyItem: 0
-  },
-  {
-    _id: 8,
-    title: 'Asus Trigger',
-    url: 'images/acb.png',
-    description: 'Designed for working in bright-light conditions, the large 14” narrow 7.3mm display of the Acer Chromebook 314 is ideal for greater productivity. Due his long battery life you can do everything.',
-    price: 999.99,
-    tam: '21.3" Full HD 1920 x 1080',
-    procesador: 'Intel Core i51035G4 1.1GHz' ,
-    ram: '32Gb RAM ' ,
-    disco: '128 GB SSD',
-    qtyItem: 0
-  }
-];
-
-*/
-Model.getProducts = function(){
-  result =  Product.find();
-  
-  return result;
- }
-
-Model.order=[{}];
-/*
-Model.users = [{
-  _id: 100, email: 'alberto@gmail.com',
-  password: 'admin',
-  name: 'Alberto',
-  surname: 'Novillo',
-  birth: '1998-09-12',
-  address: 'ESII, UCLM',
-  shoppingCart: { items: [], qty:0, total:0, subtotal:0, tax:0 },
-  orders: []
-}];
-*/
-idCounter = 101;
-
+Model.order = [{}];
 Model.user = null;
+
+
+Model.getProducts = function () {
+  result = Product.find();
+  return result;
+}
 
 Model.signin = function (email, password) {
   return User.findOne({ email, password });
-  }
+}
 
 Model.signup = function (name, surname, address, birth, email, password) {
   return User.findOne({ email })
@@ -140,239 +24,197 @@ Model.signup = function (name, surname, address, birth, email, password) {
         var user = new User({ email, password, name, surname, birth, address, shoppingCart: cart });
         return cart.save().then(function () { return user.save(); })
       } else return null;
-  })} 
+    })
+}
 
 Model.signout = function () {
   Model.user = null;
-  }
-
-
-
-
-
-Model.purchase = function (date, address, cardNumber, cardOwner, uid) {
-  
-  return Promise.all([Model.getUserById(uid), Model.getCartByUserId(uid)])
-  .then(function (results) {
-    var user = results[0];
-    var cart = results[1];
-    var number= new Date().getTime();
-    
-  var order = new Order({ 
-    'number': number,
-    'date': date,
-    'cardNumber': cardNumber,
-    'cardOwner': cardOwner,
-    'user': user,
-    'address':address,
-    'qty': cart.qty,
-    'total': cart.total,
-    'subtotal': cart.subtotal,
-    'tax': cart.tax,
-    'items':cart.items
-    
-   })
-   return order.save()
-
-  }).catch(function (errors) { console.error(errors); return null; })
-
 }
 
+Model.purchase = function (date, address, cardNumber, cardOwner, uid) {
 
+  return Promise.all([Model.getUserById(uid), Model.getCartByUserId(uid)])
+    .then(function (results) {
+      var user = results[0];
+      var cart = results[1];
+      var number = new Date().getTime();
+
+      var order = new Order({
+        'number': number,
+        'date': date,
+        'cardNumber': cardNumber,
+        'cardOwner': cardOwner,
+        'user': user,
+        'address': address,
+        'qty': cart.qty,
+        'total': cart.total,
+        'subtotal': cart.subtotal,
+        'tax': cart.tax,
+        'items': cart.items
+
+      })
+      return order.save()
+    }).catch(function (errors) { console.error(errors); return null; })
+}
 
 Model.resetCart = function (uid) {
   return Model.getUserByIdWithCart(uid)
-  .then(function (user) {
-    user.shoppingCart.items =[];
-    user.shoppingCart.qty = 0;
-    user.shoppingCart.subtotal = 0;
-    user.shoppingCart.tax = 0;
-    user.shoppingCart.total = 0;
-    return user.shoppingCart.save()
-   });
-
+    .then(function (user) {
+      user.shoppingCart.items = [];
+      user.shoppingCart.qty = 0;
+      user.shoppingCart.subtotal = 0;
+      user.shoppingCart.tax = 0;
+      user.shoppingCart.total = 0;
+      return user.shoppingCart.save()
+    });
 }
 
-Model.getOrders=function(uid){
+Model.getOrders = function (uid) {
   return Model.getUserByIdWithOrders(uid)
-  .then(function (userWOrders){
-    return userWOrders.orders;
-  })
-  };
-
-Model.getOrder = function (number, uid){
-  return this.getUserById(uid)
-  .then(function(user){
-    console.log(user)
-    return Order.findOne({ 'number': number, 'user':user._id})
-    .then(function (order){
-      return order;
+    .then(function (userWOrders) {
+      return userWOrders.orders;
     })
-  })
+};
 
+Model.getOrder = function (number, uid) {
+  return this.getUserById(uid)
+    .then(function (user) {
+      console.log(user)
+      return Order.findOne({ 'number': number, 'user': user._id })
+        .then(function (order) {
+          return order;
+        })
+    })
 }
 
-  Model.getUserById = function (userid) {
-    //
-    return User.findById(userid);
-  }
+Model.getUserById = function (userid) {
+  return User.findById(userid);
+}
 
+Model.getUserByIdWithCart = function (userid) {
+  return User.findById(userid).populate('shoppingCart');
+}
 
-  Model.getUserByIdWithCart = function (userid) {
-    //
-    return User.findById(userid).populate('shoppingCart');
-  }
-
-  Model.getUserByIdWithOrders = function (uid){
-    return Model.getUserById(uid).populate('orders');;
-   }
-  Model.getUserCartQty = function (userid) {
-    //
-    return User.findById(userid).populate({ path: 'shoppingCart', select: 'qty' })
-  }
-
-
-
-
-  Model.getProductById = function (pid) {
-    //
-      return Product.findById(pid);
-  }
-
-
-  Model.buy = function (uid, pid) {
-    //
-    return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
-      .then(function (results) {
-        var product = results[0];
-        var user = results[1];
-        console.log(user.shoppingCart)
-        if (user && product) {
-          var item = null;
-          for (var i = 0; i < user.shoppingCart.items.length; i++) {
-            if (user.shoppingCart.items[i].product == pid) {
-              item = user.shoppingCart.items[i];
-              user.shoppingCart.items.remove(item);
-            }
-          }
-          if (!item) { item = { qty: 0 }; }
-          item.product = product._id;
-          item.qty++;
-          item.title = product.title;     
-          
-          item.price = product.price;
-          item.total = item.qty * item.price;
-          user.shoppingCart.items.push(item);
-          Model.updateShoppingCart(user);
-          return user.shoppingCart.save()
-            .then(function (result) { return result });
-        } else return null;
-      }).catch(function (errors) { console.error(errors); return null; })
-  }
-  
-  
-
-  Model.updateShoppingCart = function (user) {
-    //
-    user.shoppingCart.qty = 0;
-    user.shoppingCart.subtotal = 0;
-   for (var i = 0; i <  user.shoppingCart.items.length; i++) {
-      user.shoppingCart.qty =  user.shoppingCart.qty +  user.shoppingCart.items[i].qty;
-      user.shoppingCart.subtotal =  user.shoppingCart.subtotal +  user.shoppingCart.items[i].total;
-   }
-   
-    user.shoppingCart.tax =  user.shoppingCart.subtotal * 0.21;
-    user.shoppingCart.total =  user.shoppingCart.subtotal +  user.shoppingCart.tax;
-    
- }
- 
-
- Model.getCartByUserId = function (uid) {
-  //
-  return this.getUserByIdWithCart(uid)
-  .then(function (sc) { 
-    return sc.shoppingCart;
-  });
-
-  }
-
- Model.getOrdersByUserId = function (uid){
-   //need to reswatch
+Model.getUserByIdWithOrders = function (uid) {
   return Model.getUserById(uid).populate('orders');;
- }
+}
 
-  Model.removeOne = function (uid, pid) {
-    //
-    return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
-      .then(function (results){
-        var product = results[0];
-        var user = results[1];
-        if (!product || !user) return null;
-        return Cart.findById(user.shoppingCart)
-          .then( function (cart){
-            
-            for (var i = 0; i < cart.items.length; i++) {
-              if (cart.items[i].product == pid) {
-                var item = cart.items[i];
-                item.qty = item.qty - 1;
-                if (cart.items[i].qty < 1)
-                cart.items.splice(i, 1);
-                else {
-                  item.price = product.price;
-                  item.total = item.qty * item.price;
-                }  }  }
-               
-            cart = Model.updateOneCart(cart)
-            return cart.save();
-          })
-      }).catch(function (errors) { console.error(errors); return null; })
+Model.getUserCartQty = function (userid) {
+  return User.findById(userid).populate({ path: 'shoppingCart', select: 'qty' })
+}
+
+Model.getProductById = function (pid) {
+  return Product.findById(pid);
+}
+
+Model.buy = function (uid, pid) {
+  return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
+    .then(function (results) {
+      var product = results[0];
+      var user = results[1];
+      if (user && product) {
+        var item = null;
+        for (var i = 0; i < user.shoppingCart.items.length; i++) {
+          if (user.shoppingCart.items[i].product == pid) {
+            item = user.shoppingCart.items[i];
+            user.shoppingCart.items.remove(item);
+          }
+        }
+        if (!item) { item = { qty: 0 }; }
+        item.product = product._id;
+        item.qty++;
+        item.title = product.title;
+        item.price = product.price;
+        item.total = item.qty * item.price;
+        user.shoppingCart.items.push(item);
+        Model.updateShoppingCart(user);
+        return user.shoppingCart.save()
+          .then(function (result) { return result });
+      } else return null;
+    }).catch(function (errors) { console.error(errors); return null; })
+}
+
+Model.updateShoppingCart = function (user) {
+  user.shoppingCart.qty = 0;
+  user.shoppingCart.subtotal = 0;
+  for (var i = 0; i < user.shoppingCart.items.length; i++) {
+    user.shoppingCart.qty = user.shoppingCart.qty + user.shoppingCart.items[i].qty;
+    user.shoppingCart.subtotal = user.shoppingCart.subtotal + user.shoppingCart.items[i].total;
   }
-  Model.removeAll = function (uid, pid) {
-    return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
-    .then(function (results){
+
+  user.shoppingCart.tax = user.shoppingCart.subtotal * 0.21;
+  user.shoppingCart.total = user.shoppingCart.subtotal + user.shoppingCart.tax;
+}
+
+
+Model.getCartByUserId = function (uid) {
+  return this.getUserByIdWithCart(uid)
+    .then(function (sc) {
+      return sc.shoppingCart;
+    });
+}
+
+Model.getOrdersByUserId = function (uid) {
+  return Model.getUserById(uid).populate('orders');;
+}
+
+Model.removeOne = function (uid, pid) {
+  return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
+    .then(function (results) {
       var product = results[0];
       var user = results[1];
       if (!product || !user) return null;
       return Cart.findById(user.shoppingCart)
-      .then( function (cart){
-        for (var i = 0; i < cart.items.length; i++) {
-          if (cart.items[i].product == pid) {
-            cart.items.splice(i, 1);
-          }
-        } 
-        cart = Model.updateOneCart(cart)
-        return cart.save();
-      })
-    }).catch(function (errors) { console.error(errors); return null; })
-    /*var product = Model.getProductById(pid);
-    var user = Model.getUserById(uid);
-    
+        .then(function (cart) {
 
-    for (var i = 0; i < user.shoppingCart.items.length; i++) {
-      if (user.shoppingCart.items[i].product == pid) {
-        user.shoppingCart.items.splice(i, 1);
-      }
-    }
-    
-    this.updateShoppingCart(user);
-    return user.shoppingCart;
-    */
-  };
-  Model.updateOneCart = function(cart){
-    //
-    cart.qty = 0;
-    cart.subtotal = 0;
-    for (var i = 0; i <  cart.items.length; i++) {
-      cart.qty =  cart.qty +  cart.items[i].qty;
-      cart.subtotal =  cart.subtotal +  cart.items[i].total;
-     }
-   
-     cart.tax = cart.subtotal * 0.21;
-     cart.total =  cart.subtotal +  cart.tax;
-     return cart;
+          for (var i = 0; i < cart.items.length; i++) {
+            if (cart.items[i].product == pid) {
+              var item = cart.items[i];
+              item.qty = item.qty - 1;
+              if (cart.items[i].qty < 1)
+                cart.items.splice(i, 1);
+              else {
+                item.price = product.price;
+                item.total = item.qty * item.price;
+              }
+            }
+          }
+
+          cart = Model.updateOneCart(cart)
+          return cart.save();
+        })
+    }).catch(function (errors) { console.error(errors); return null; })
+}
+Model.removeAll = function (uid, pid) {
+  return Promise.all([Model.getProductById(pid), Model.getUserByIdWithCart(uid)])
+    .then(function (results) {
+      var product = results[0];
+      var user = results[1];
+      if (!product || !user) return null;
+      return Cart.findById(user.shoppingCart)
+        .then(function (cart) {
+          for (var i = 0; i < cart.items.length; i++) {
+            if (cart.items[i].product == pid) {
+              cart.items.splice(i, 1);
+            }
+          }
+          cart = Model.updateOneCart(cart)
+          return cart.save();
+        })
+    }).catch(function (errors) { console.error(errors); return null; })
+
+};
+Model.updateOneCart = function (cart) {
+  cart.qty = 0;
+  cart.subtotal = 0;
+  for (var i = 0; i < cart.items.length; i++) {
+    cart.qty = cart.qty + cart.items[i].qty;
+    cart.subtotal = cart.subtotal + cart.items[i].total;
   }
 
-
+  cart.tax = cart.subtotal * 0.21;
+  cart.total = cart.subtotal + cart.tax;
+  return cart;
+}
 
 module.exports = Model;
-
